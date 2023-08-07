a6bc00e12dd7f43a37697659052f7ee01a4c6d7a
juanbotaro <juanbotaro@gmail.com>
2023-08-07 20:27:35 -0300

    Initial commit

---
+++ src/ej02_guia1_sc/Ej02_Guia1_SC.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ej02_guia1_sc;
+
+/**
+ *
+ * @author Maquina3
+ */
+public class Ej02_Guia1_SC {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
