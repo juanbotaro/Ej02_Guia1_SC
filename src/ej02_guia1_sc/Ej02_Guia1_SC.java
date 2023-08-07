0a89be5efdfa7fc4b136d78dd64f05f3b392070e
Gabyg29 <gimgari@hotmail.com.ar>
2023-08-07 20:44:34 -0300

    Gabriela
    Salude

--- src/ej02_guia1_sc/Ej02_Guia1_SC.java
+++ src/ej02_guia1_sc/Ej02_Guia1_SC.java
@@ -6,6 +6,7 @@ public class Ej02_Guia1_SC {
     public static void main(String[] args) {
         System.out.println("hola soy fernanddo");
         System.out.println("hola soy Claudio");
+        System.out.println("hola soy Gabriela");
     }
     
 }
