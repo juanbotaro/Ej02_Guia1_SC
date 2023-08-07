994b31fb605ab4e39e4642e526b5fbef7bfc334d
Claudio Alejandro Ledesma <140399822+CALedesma87@users.noreply.github.com>
2023-08-07 20:40:06 -0300

    Update Ej02_Guia1_SC.java
    Saludo mio

--- src/ej02_guia1_sc/Ej02_Guia1_SC.java
+++ src/ej02_guia1_sc/Ej02_Guia1_SC.java
@@ -5,6 +5,7 @@ public class Ej02_Guia1_SC {
 
     public static void main(String[] args) {
         System.out.println("hola soy fernanddo");
+        System.out.println("hola soy Claudio");
     }
     
 }
