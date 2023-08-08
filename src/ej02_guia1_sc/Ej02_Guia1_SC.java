fe56aedb209e182436df29b822536f236ab8756b
CALedesma87 <Alejandro_ledesma_87@live.com.ar>
2023-08-08 12:40:46 -0300

    cambie color a mi nombre

--- src/ej02_guia1_sc/Ej02_Guia1_SC.java
+++ src/ej02_guia1_sc/Ej02_Guia1_SC.java
@@ -5,7 +5,7 @@ public class Ej02_Guia1_SC {
 
     public static void main(String[] args) {
         System.out.println("\033[31mhola soy fernanddo");
-        System.out.println("hola soy Claudio");
+        System.out.println("\033[32mhola soy Claudio");
         System.out.println("hola soy Gabriela");
 //        hola soy hernan estoy modificadondo desde la web
         System.out.println("Hola, soy Martin");
